a34d1a7f889515672ceceda7afbe6de8084dcfc7
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-06-23 17:48:17 -0700

    setting up environment and dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from "cors";
 
 const app = express()
 const PORT = process.env.PORT || 4500
 app.get('/', (req, res) => {
     res.send('Backend is working')
 })
+
+
+app.use(cors());
 app.use(express.json())
 app.listen(PORT,
   console.log(`Server running on http://localhost:${PORT}`)
