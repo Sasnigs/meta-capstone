de0fed74c055fc9915f08bbb9f1a0313471d5a16
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-06-27 16:06:30 -0700

    naming of OMDB api functions parameters

--- frontend/utils/omdbUtils.js
+++ frontend/utils/omdbUtils.js
@@ -1,14 +1,14 @@
 import { OMBD_BASE_URL } from "../src/data/data";
 const API_KEY = import.meta.env.VITE_OMBD_API_KEY;
 
-export async function getMovieById(id) {
-  const res = await fetch(`${OMBD_BASE_URL}?apikey=${API_KEY}&i=${id}`);
+export async function getMovieById(movieId) {
+  const res = await fetch(`${OMBD_BASE_URL}?apikey=${API_KEY}&i=${movieId}`);
   const data = await res.json();
   return data;
 }
 
-export async function getMoviesByTitle(value) {
-  const res = await fetch(`${OMBD_BASE_URL}?apikey=${API_KEY}&s=${value}`);
+export async function getMoviesByTitle(movieTitle) {
+  const res = await fetch(`${OMBD_BASE_URL}?apikey=${API_KEY}&s=${movieTitle}`);
   const data = await res.json();
   return data.Search;
 }
