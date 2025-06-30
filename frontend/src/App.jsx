f90e6f676cc440b9cdb2a186e252d130ec111ec7
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-06-30 08:24:49 -0700

    Implemented check session for users to make sure user has a valid session. Added authentication for routes to homepage and movie forum  to prevent unathuorised access.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { BASE_URL } from "./data/data";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Homepage from "./Homepage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MovieForum from "./MovieForum";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [checkingSession, setCheckingSession] = useState(true);
+
+  async function checkSession() {
+    try {
+      const res = await fetch(`${BASE_URL}/me`, { credentials: "include" });
+      const data = await res.json();
+      if (res.ok && data.loggedIn) {
+        setUser(data);
+      } else {
+        setUser(null);
+      }
+    } catch (error) {
+      setUser(null);
+    }
+    setCheckingSession(false);
+  }
+  useEffect(() => {
+    checkSession();
+  }, []);
+  if (checkingSession) return <div>Loading...</div>;
+
   function navigate(url) {
     window.location.href = url;
   }
@@ -23,10 +45,28 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="signUp" element={<SignUp />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/movie/:id" element={<MovieForum />} />
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate to="/homepage" replace />
+            ) : (
+              <SignIn checkSession={checkSession} />
+            )
+          }
+        />
+        <Route
+          path="signUp"
+          element={
+            user ? (
+              <Navigate to="homepage" replace />
+            ) : (
+              <SignUp checkSession={checkSession} />
+            )
+          }
+        />
+        <Route path="/homepage" element={user ? <Homepage /> : <Navigate to="/" replace />} />
+        <Route path="/movie/:id" element={user ? <MovieForum user={user} /> : <Navigate to="/" replace /> } />
       </Routes>
     </>
   );
