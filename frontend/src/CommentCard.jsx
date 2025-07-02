833dc01ef6e512eb2300563d92c06970a5d8c674
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-07-01 18:26:37 -0700

    modified style to comments section and rm hardcoded values

--- frontend/src/CommentCard.jsx
+++ frontend/src/CommentCard.jsx
@@ -1,23 +1,18 @@
 import "./commentcard.css";
 
-export default function CommentCard() {
+export default function CommentCard({commentDetails}) {
   return (
     <div>
       <div className="comment-cont">
         <div className="user-area">
           <div>
-            <p>@Username</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
-              veritatis error quisquam id facere, incidunt ex nostrum
-              distinctio, velit doloremque provident debitis delectus nesciunt
-              dolore. Officia accusantium assumenda nemo esse.
-            </p>
+            <p>@{commentDetails.username}</p>
+            <p>{commentDetails.text}</p>
           </div>
         </div>
         <div className="up-down-vote">
-          <p>⬆0</p>
-          <p>⬇20</p>
+          <p>⬆{commentDetails.upVote}</p>
+          <p>⬇{commentDetails.downVote}</p>
         </div>
       </div>
     </div>
