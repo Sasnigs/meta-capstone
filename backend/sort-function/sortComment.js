3ad1468f5c77fb504d4af4342584a7d10361f014
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-07-03 12:52:46 -0700

    implemented sort by controversial

--- backend/sort-function/sortComment.js
+++ backend/sort-function/sortComment.js
@@ -1,14 +1,33 @@
+const sortBy = {
+    most_loved: "Most Loved",
+    most_hated: "Most Hated",
+    most_recent: "Most Recent",
+    oldest_user: "Oldest User",
+    controversial: "Controversial",
+    trending: "Trending",
+    net_usefule: "Net Useful"
+}
+
 export function sortComment(array, sortType) {
-  if (sortType === "most loved") {
+  if (sortType === sortBy.most_loved ) {
     array.sort((x, y) => x.upVotes - y.upVotes);
   }
-  if (sortType === "most hated") {
+  else if (sortType === sortBy.most_hated) {
     array.sort((x, y) => x.downVotes - y.downVotes);
   }
-  if (sortType === "most recent") {
+  else if (sortType === sortBy.most_recent) {
     array.sort((x, y) => new Date(x.createdAt) - new Date(y.createdAt));
   }
-  if (sortType === "oldest users") {
+  else if (sortType === sortBy.oldest_user) {
     array.sort((x, y) => x.user.createdAt - y.user.createdAt);
   }
+  else if (sortType === sortBy.controversial){
+    array.map((comment) => {
+        let controversial_score = abs(1 - (comment.upVotes / comment.downVotes))
+        comment.controversial = controversial_score
+    })
+    array.sort((x,y) => x.controversial - y.controversial)
+  }
 }
+// jin: use elif/enum for conditional statements
+// todo: check for api that shows trailer
