7c5ae939bc9a1169d08386261ac804413aeb0d5b
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-07-02 18:00:14 -0700

    implemented tooltip and added style for comment card

--- frontend/src/CommentCard.jsx
+++ frontend/src/CommentCard.jsx
@@ -1,14 +1,29 @@
 import "./commentcard.css";
 
-export default function CommentCard({commentInfo}) {
+export default function CommentCard({ commentInfo }) {
   return (
     <div>
       <div className="comment-cont">
         <div className="user-area">
-          <div>
-            <p>@{commentInfo.user.username}</p>
-            <p>{commentInfo.message}</p>
+          <div className="tooltip-wrapper">
+            <span className="username">@{commentInfo.user.username}</span>
+            <div className="tooltip">
+              <p>
+                <strong>Username:</strong> {commentInfo.user.username}
+              </p>
+              <p>
+                <strong>Joined: </strong>
+                {new Date(commentInfo.user.createdAt).toLocaleDateString(
+                  "en-US",
+                  {
+                    year: "numeric",
+                    month: "long",
+                  }
+                )}
+              </p>
+            </div>
           </div>
+          <p>{commentInfo.message}</p>
         </div>
       </div>
     </div>
