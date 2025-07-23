041c5e24864fa5e98d025551b4ac422240e118ed
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-07-23 12:34:19 -0700

    review change

--- backend/clean-string/cleanString.js
+++ backend/clean-string/cleanString.js
@@ -2,10 +2,14 @@ export function removePunctuation(str) {
   let result = '';
   for (let i = 0; i < str.length; i++) {
     let character = str.charAt(i);
-    if (character.charCodeAt(0) >= 97 && character.charCodeAt(0) <= 122) {
+    let charCode = character.charCodeAt(0);
+    if (
+      (charCode >= 65 && charCode <= 90) ||    // A–Z
+      (charCode >= 97 && charCode <= 122) ||   // a–z
+      charCode === 32                          // space
+    ) {
       result += character;
     }
   }
   return result;
 }
-
